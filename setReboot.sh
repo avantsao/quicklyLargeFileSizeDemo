3666a0928c08779918d7bfbf245645dac58d656e
avantsao <avan_tsao@wistron.com>
2018-08-09 10:21:40 +0800

    Trial v0.1 for single disk partition

---
+++ setReboot.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#check the origional disk size
+#append to large the test file for using avaiable disk space#
+#check available disk size
+#if available space is zero, do reboot
+cp script/count.sh /tmp/
+
